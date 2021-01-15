fdd1fe1ef5752586b0b2e4da4d77180f28aee172
Jonathan Slauter <jonathanslauter@gmail.com>
2021-01-14 19:05:31 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 3000
+const mongoose = require('mongoose')
+const Product = require('./models/product')
+
+mongoose.connect('mongodb://localhost:27017/farmstand', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("mongo connection open")
+  })
+  .catch(err => {
+    console.log("mongo connection error")
+    console.log(err)
+  })
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find({})
+  res.render('products/index', { products })
+})
+
+
+
+app.listen(PORT, () => {
+  console.log("app is listening on port 3000")
+})
