88e8ea6ec4389be1df5ec7d356430d1f2db5f8ff
Jonathan Slauter <jonathanslauter@gmail.com>
2021-01-16 21:52:19 -0800

    added editing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const PORT = 3000
 const mongoose = require('mongoose')
 const Product = require('./models/product')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/farmstand', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -16,7 +17,9 @@ mongoose.connect('mongodb://localhost:27017/farmstand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({})
@@ -25,6 +28,12 @@ app.get('/products', async (req, res) => {
 
 app.get('/products/new', (req, res) => {
   res.render('products/new')
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findById(id)
+  res.render('products/edit', { product })
 })
 
 app.post('/products', async (req, res) => {
@@ -38,6 +47,12 @@ app.get('/products/:id', async (req, res) => {
   const { id } = req.params
   const product = await Product.findById(id)
   res.render('products/show', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+  res.redirect(`/products/${product._id}`)
 })
 
 
